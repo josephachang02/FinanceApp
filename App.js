87c2029c64601e1c750144d4e07b83352a4cf9b4
JoeChang <josephachang02@gmail.com>
2023-08-28 15:22:59 -0700

    project1322

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+//Navigation of App.HTML to 'NeedsObligation.html' through Submit Event
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('incomeInitial');
     form.addEventListener('submit', function(event) {
@@ -5,5 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
         const totalIncome = document.getElementById('TotalIncome').value;
         // Assuming you want to navigate to 'result.html'
         window.location.href = `NeedsObligation.html?income=${totalIncome}`;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('incomeNecessities');
+    const totalAmountSpan = document.getElementById('totalAmount');
+    const housingExpensesInput = document.getElementById('housingExpenses');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+
+        const housingExpenses = parseFloat(housingExpensesInput.value);
+        const currentTotal = parseFloat(totalAmountSpan.innerText);
+        const newTotal = currentTotal + housingExpenses;
+
+        totalAmountSpan.innerText = newTotal.toFixed(2); // Update the total with two decimal places
     });
 });
